20167dfd0fef7a298fafb8361c3c3153d67d7e66
Preuß <joshua.preuss2000@gmx.de>
2020-04-16 16:49:40 +0200

    added content type for json response

--- src/main/java/covid19Tracker/infrastructure/web/RegisterEndpoint.java
+++ src/main/java/covid19Tracker/infrastructure/web/RegisterEndpoint.java
@@ -2,6 +2,7 @@ package covid19Tracker.infrastructure.web;
 
 import covid19Tracker.application.AccountService;
 import covid19Tracker.domain.User;
+import org.eclipse.jetty.http.MimeTypes;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.json.JSONObject;
@@ -24,19 +25,22 @@ public class RegisterEndpoint extends AbstractHandler {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Headers", "*");
 
+        String registration = "";
 
         if (request.getParameter("registration") != null) {
-            String registration = request.getParameter("registration");
+            registration = request.getParameter("registration");
         }
 
-        User user = accountService.register();
-        if(user == null){
-            response.setStatus(500);
-        }
-
-        JSONObject data = new JSONObject().put("userID", user.userID);
-        data.put("deleteCode", user.deleteCode);
+        if (registration.equals("true")) {
+            User user = accountService.register();
+            if (user == null) {
+                response.setStatus(500);
+            }
+        JSONObject data = new JSONObject().put("id", user.userID);
+        data.put("deletecode", user.deleteCode);
+        response.setContentType(MimeTypes.Type.APPLICATION_JSON_UTF_8.asString());
         response.getWriter().print(data);
+        }
 
         baseRequest.setHandled(true);
         System.out.println("Register Page is running...");
