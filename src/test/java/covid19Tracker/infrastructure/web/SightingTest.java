c120be3592d1cae4877c2719db2d34a6512fd0c3
Preuß <joshua.preuss2000@gmx.de>
2020-04-28 16:19:59 +0200

    tests for sighting

---
+++ src/test/java/covid19Tracker/infrastructure/web/SightingTest.java
@@ -0,0 +1,53 @@
+package covid19Tracker.infrastructure.web;
+
+import covid19Tracker.domain.Sighting;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SightingTest {
+
+    private Sighting sighting;
+    private Sighting otherSighting;
+
+
+    @Test
+    public void Sighting_should_returnFalse_for_similarPlace_and_differentTime() throws IOException {
+        otherSighting = new Sighting(52.1313922, 13.216249399999999, new java.util.Date());
+        sighting = new Sighting(52.13139221, 13.216249399999999, new java.util.Date(System.currentTimeMillis()-24*60*60*1000));
+
+        System.out.println("First Test: ");
+        assertFalse(sighting.closeTo(otherSighting));
+    }
+
+    @Test
+    public void Sighting_should_returnFalse_for_differentPlace_and_similarTime() throws IOException {
+        otherSighting = new Sighting(52.1313922, 13.216249399999999, new java.util.Date());
+        sighting = new Sighting(52.1313922, 5.216249399999999, new java.util.Date(System.currentTimeMillis()-3*60*1000));
+
+        System.out.println("Second Test: ");
+        assertFalse(sighting.closeTo(otherSighting));
+    }
+
+    @Test
+    public void Sighting_should_returnFalse_for_differentPlace_and_differentTime() throws IOException {
+        otherSighting = new Sighting(52.1313922, 13.216249399999999, new java.util.Date());
+        sighting = new Sighting(52.1313922, 5.216249399999999, new java.util.Date(System.currentTimeMillis()-24*60*60*1000));
+
+        System.out.println("Third Test: ");
+        assertFalse(sighting.closeTo(otherSighting));
+    }
+
+    @Test
+    public void Sighting_should_returnTrue_for_similarPlace_and_similarTime() throws IOException {
+        otherSighting = new Sighting(52.1313922, 13.216249399999999, new java.util.Date());
+        sighting = new Sighting(52.13139221, 13.216249399999999, new java.util.Date(System.currentTimeMillis()-3*60*1000));
+
+        System.out.println("Fourth Test: ");
+        assertTrue(sighting.closeTo(otherSighting));
+    }
+}
