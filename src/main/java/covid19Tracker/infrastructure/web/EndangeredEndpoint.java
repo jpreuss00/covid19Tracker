efc26c4b04551d7e91c7172f88e42686ed64cbc7
Preuß <joshua.preuss2000@gmx.de>
2020-04-29 14:13:47 +0200

    added endangered enpoint to check if person is in radius of infected person

---
+++ src/main/java/covid19Tracker/infrastructure/web/EndangeredEndpoint.java
@@ -0,0 +1,53 @@
+package covid19Tracker.infrastructure.web;
+
+import covid19Tracker.application.AccountService;
+import covid19Tracker.application.SightingRepository;
+import covid19Tracker.application.SightingService;
+import covid19Tracker.domain.Sighting;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.json.JSONObject;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+public class EndangeredEndpoint extends AbstractHandler {
+
+    private final CorsHandler corsHandler;
+    private final SightingService sightingService;
+
+    public EndangeredEndpoint(SightingService sightingService, CorsHandler corsHandler) {
+        this.sightingService = sightingService;
+        this.corsHandler = corsHandler;
+    }
+
+    @Override
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        corsHandler.handleCors(request, response);
+        baseRequest.setHandled(true);
+
+        double latitude = 0;
+        double longitude = 0;
+
+        if (request.getParameter("latitude") != null) {
+            latitude = Double.parseDouble(request.getParameter("latitude"));
+        }
+        if (request.getParameter("longitude") != null) {
+            longitude = Double.parseDouble(request.getParameter("latitude"));
+        }
+        Date date = new Date();
+
+        if (latitude != 0 && longitude != 0) {
+            Sighting sighting = new Sighting(latitude, longitude, date);
+            if(sightingService.isEndangered(sighting)){
+                response.setStatus(200);
+            } else {
+                response.setStatus(204);
+            }
+        }
+        System.out.println("Endangered Page is running...");
+    }
+
+}
